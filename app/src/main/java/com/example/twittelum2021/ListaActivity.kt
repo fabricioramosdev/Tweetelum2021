eda1cf96a0af438250fccb333b5e5bc456b4a640
FABRICIO ROGERIO LOPES RAMOS <FABRIRAM@bot.ad.emb>
2021-07-31 01:41:09 -0300

    implementando CoordinatorLayout resolvendo gravity do FloatingActionButton

--- app/src/main/java/com/example/twittelum2021/ListaActivity.kt
+++ app/src/main/java/com/example/twittelum2021/ListaActivity.kt
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class ListaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
 
-        val tweets = listOf("Meu primeiro tweet", "Meu segundo tweet", "Meu terceiro tweet","Meu quarto tweet", "Meu quinto tweet", "Hoje esta frio!", "Meu sexta tweet", "Meu setimo tweet" )
+        val tweets = listOf("Meu primeiro tweet", "Meu segundo tweet", "Meu terceiro tweet",
+            "Meu quarto tweet", "Meu quinto tweet", "Hoje esta frio!", "Meu sexta tweet",
+            "Meu setimo tweet", "Hoje esta frio!", "Hoje esta frio!", "Hoje esta frio!",
+            "Hoje esta frio!", "Hoje esta frio!", "Hoje esta frio!", "Hoje esta frio!",
+            "Hoje esta frio!", "Hoje esta frio!", "Hoje esta frio!", "Hoje esta frio!")
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tweets)
         val lista_tweet = findViewById<ListView>(R.id.lista_tweet)
 
         lista_tweet.adapter = adapter
+
+
+        val fab_add = findViewById<FloatingActionButton>(R.id.fab_add)
+        
+        fab_add.setOnClickListener { Snackbar.make(it, "FAB Clicado", Snackbar.LENGTH_LONG).show()}
+
+
+
     }
 }
