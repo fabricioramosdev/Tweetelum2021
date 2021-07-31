a5e6b6f51af183cb4317f79ef8af79ad0b0db8b4
FABRICIO ROGERIO LOPES RAMOS <FABRIRAM@bot.ad.emb>
2021-07-31 01:21:10 -0300

    adicionar floatingactionbutton

--- app/src/main/java/com/example/twittelum2021/ListaActivity.kt
+++ app/src/main/java/com/example/twittelum2021/ListaActivity.kt
@@ -11,10 +11,10 @@ class ListaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
 
-        val tweets = listOf("Meu primeiro tweet", "Meu segundo tweet", "Meu terceiro tweet","Meu quarto tweet", "Meu quinto tweet", "Hoje esta frio!" )
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, tweets)
-        val lista = findViewById<ListView>(R.id.lista_tweet)
+        val tweets = listOf("Meu primeiro tweet", "Meu segundo tweet", "Meu terceiro tweet","Meu quarto tweet", "Meu quinto tweet", "Hoje esta frio!", "Meu sexta tweet", "Meu setimo tweet" )
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tweets)
+        val lista_tweet = findViewById<ListView>(R.id.lista_tweet)
 
-        lista.adapter = adapter
+        lista_tweet.adapter = adapter
     }
 }
